cb490edcdbaf3561b7c8cbf1ec89ee7fabf070bb
gyudongjeong <kudong0826@gmail.com>
2026-06-25 17:40:34 +0900

    First SBB Practice Setting Commit

---
+++ src/main/java/com/mysite/sbb/HelloController.java
@@ -0,0 +1,19 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class HelloController {
+
+	@GetMapping("/hello")
+	@ResponseBody
+	public String hello() {
+		return "Hello World";
+	} //hello 메서드
+	
+	
+} //class
