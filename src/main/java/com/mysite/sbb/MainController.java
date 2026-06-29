fe77a2ff9ae1b7dff7e6879b1458737a8810c95d
gyudongjeong <kudong0826@gmail.com>
2026-06-29 17:51:57 +0900

    H2 Database Configuration And Add Comments

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -8,20 +8,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
 
-	@GetMapping("/sbb") // GET 방식의 HTTP 요청을 처리함. // 도메인명 & 포트는 작성 X. → 서버 설정에 따라 변하기 때문.
+	@GetMapping("/sbb") // GET 방식의 HTTP 요청을 처리함.
 	@ResponseBody // 메서드의 반환 값을 뷰(템플릿)로 해석하지 않고, HttpMessageConverter를 통해 HTTP Response Body에 직접 작성함.
 	public String index() {
 		return "안녕하세요 sbb에 오신 것을 환영합니다.";
 	} //index 메서드	
 	
-} //class
+} //class: Controller 클래스
 
 
 /*
  	HTTP Status Code
- 	[1] 404 : 요청한 URL에 해당하는 리소스(페이지, API)를 Server에서 못 찾음.
+ 	[1] 404 : 브라우저가 요청한 URL에 해당하는 리소스(페이지, API)를 Server에서 못 찾음.
  		(1) 해결법 : Controller에 해당 URL에 대한 매핑 추가하기.
- 			① URL 매핑 : 'URL'과 'Controller 메서드'를 연결하는 것.
+ 			① URL 매핑 : 'URL'과 'Controller 메서드'를 연결하는 것. 이때, 서버 설정에 따라 변하는 도메인명과 포트는 작성 X.
 	
 	[2] 500 : Controller 메서드 실행 중 예외가 발생한 경우 or 응답(반환값) 처리 과정에서 오류가 발생한 경우.
 		(1) 해결법 : 클라이언트에게 응답 반환하기.
--- src/main/java/com/mysite/sbb/SbbApplication.java
+++ src/main/java/com/mysite/sbb/SbbApplication.java
@@ -3,11 +3,18 @@ package com.mysite.sbb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * @SpringBootApplication = @SpringBootConfiguration + @EnableAutoConfiguration + @ComponentScan
+ * @SpringBootConfiguration: 해당 클래스를 Spring의 설정 클래스로 등록함.
+ * @EnableAutoConfiguration: classpath의 라이브러리 및 설정(ex: application.properties)을 기반으로 필요한 Bean을 자동 구성함.
+ * @ComponentScan: 해당 클래스의 패키지 및 그 하위 패키지를 스캔해서 컴포넌트 스캔 대상 어노테이션(ex: @Component, @Service, @Repository, @Controller 등)이 붙은 클래스를 자동으로 Bean으로 등록함.
+*/
+
 @SpringBootApplication
-public class SbbApplication {
+public class SbbApplication { //Spring Boot 애플리케이션의 시작 클래스(Entry Point)
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbbApplication.class, args);
 	}
 
-}
+} //class: '프로젝트명 + Application.java' 파일
