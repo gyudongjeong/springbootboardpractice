05604903a63a95bec5182cdc683c5e62f27cf8ee
gyudongjeong <kudong0826@gmail.com>
2026-07-01 17:20:38 +0900

    Question Repository Test

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,13 +1,34 @@
 package com.mysite.sbb;
 
+import java.time.LocalDateTime;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SbbApplicationTests {
 
+	@Autowired //@Autowired: Spring Container에서 주입 대상 타입과 일치하는 Bean을 찾아서 자동으로 의존성을 주입해줌.
+	private QuestionRepository questionRepository;
+	
 	@Test
-	void contextLoads() {
+	void testJpa() {
+		
+		//1번째 질문 저장
+		Question q1 = new Question();
+		q1.setSubject("sbb가 무엇인가요?");
+		q1.setContent("sbb에 대해서 알고 싶습니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q1);
+		
+		//2번째 질문 저장
+		Question q2 = new Question();
+		q2.setSubject("스프링부트 모델 질문입니다.");
+		q2.setContent("id는 자동으로 생성되나요?");
+		q2.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q2);
+		
 	}
 
 }
